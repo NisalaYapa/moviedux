3c4d270a26ee12a1c52cd048c02fe84b44bde089
Nisala Yapa <nisalayapanc@gmail.com>
2025-04-02 00:41:42 +0530

    added movie grid component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import './styles.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import MovieGrid from './components/MoviesGrid';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className='container'>
         <Header></Header>
       </div>
+      <MovieGrid></MovieGrid>
       <footer className='footer'>
         <Footer></Footer>
       </footer>
